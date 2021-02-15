f1a8ff9a1928084766f8906220a7320a28725d60
GTA20HayashiSoui <hayashisoui@icloud.com>
2021-02-15 12:12:31 +0900

    Stage42を追加したよ❤

---
+++ Assets/GPU_RETURNS/@Project/Scripts/HayashiSouiScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HayashiSouiScript : MonoBehaviour
+{
+    string tachaScene = "Stage10";
+    // Start is called before the first frame update
+    void Start()
+    {
+        //tachaScene.name = "Stage10";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        SceneManager.LoadScene(tachaScene);
+    }
+}
